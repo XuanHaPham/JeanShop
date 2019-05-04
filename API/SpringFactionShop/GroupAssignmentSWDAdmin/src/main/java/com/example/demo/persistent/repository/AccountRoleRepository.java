72a106f26effbf9c9df1bb034d5a14fe809522fc
HoangNhutVu <haptxse62665@fpt.edu.vn>
2019-05-05 04:01:25 +0700

    Update Account Auth API (Add role : 1-User, 2-Admin in table 'role' by yourself)

--- API/SpringFactionShop/GroupAssignmentSWDAdmin/src/main/java/com/example/demo/persistent/repository/AccountRoleRepository.java
+++ API/SpringFactionShop/GroupAssignmentSWDAdmin/src/main/java/com/example/demo/persistent/repository/AccountRoleRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.persistent.repository;
 
 import com.example.demo.persistent.entity.Account;
 import com.example.demo.persistent.entity.AccountRole;
+import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +12,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRoleRepository extends JpaRepository<AccountRole, Integer> {
+
+    @Query("SELECT a.roleID FROM AccountRole a WHERE a.accountID = :accountID AND a.status = true")
+    Integer getRoleIDByAccountID(@Param("accountID") Integer accountID);
+
 
 }
